e22ba1c77bdc6f634cb5a1428a60f8e29af54fbb
Semen Loktionov <loktionov.kh@gmail.com>
2023-08-23 16:10:57 +0300

    add query params for the list page

---
+++ src/app/[locale]/validators/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next'
+
+import { PageContainer } from '@/components'
+// import { PageContainer, Validators } from '@/components'
+import { craftPageTitle, METADATA } from '@/config'
+
+export const metadata: Metadata = {
+  ...METADATA,
+  title: craftPageTitle('Validators'),
+}
+
+export default function ValidatorsPage() {
+  return <PageContainer>{/*<Validators />*/}</PageContainer>
+}
--- src/hooks/useTablePagination.ts
+++ src/hooks/useTablePagination.ts
@@ -1,18 +1,50 @@
 'use client'
 
-import { ChangeEvent, useState } from 'react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { ChangeEvent, useCallback, useEffect, useMemo, useState } from 'react'
 
 import { CONFIG } from '@/config'
 import { SortOrder } from '@/types'
+
+const QUERY_KEYS = {
+  LIMIT: 'limit',
+  OFFSET: 'offset',
+  ORDER: 'order',
+  ORDER_BY: 'orderBy',
+}
 
 export const useTablePagination = <T = unknown>(
   initialLimit: number = CONFIG.PAGE_LIMIT,
   initialOffset = 0,
 ) => {
+  const router = useRouter()
+  const query = useSearchParams()!
+  const pathname = usePathname()
+
   const [limit, setLimit] = useState<number>(initialLimit)
   const [offset, setOffset] = useState<number>(initialOffset)
   const [order, setOrder] = useState<SortOrder>('' as SortOrder)
   const [orderBy, setOrderBy] = useState<T>('' as T)
+
+  const handlers = useMemo(
+    () => ({
+      [QUERY_KEYS.LIMIT]: (v: string) => setLimit(Number(v)),
+      [QUERY_KEYS.OFFSET]: (v: string) => setOffset(Number(v)),
+      [QUERY_KEYS.ORDER]: (v: string) => setOrder(v as SortOrder),
+      [QUERY_KEYS.ORDER_BY]: (v: string) => setOrderBy(v as T),
+    }),
+    [],
+  )
+
+  const values = useMemo(
+    () => ({
+      [QUERY_KEYS.LIMIT]: limit,
+      [QUERY_KEYS.OFFSET]: offset,
+      [QUERY_KEYS.ORDER]: order,
+      [QUERY_KEYS.ORDER_BY]: orderBy,
+    }),
+    [limit, offset, order, orderBy],
+  )
 
   const handleChangePage = (_: unknown, newPage: number) => {
     setOffset(newPage * limit)
@@ -28,6 +60,36 @@ export const useTablePagination = <T = unknown>(
     setOrder(isAsc ? 'desc' : 'asc')
     setOrderBy(sortOrderBy)
   }
+
+  useEffect(() => {
+    for (const [key, handler] of Object.entries(handlers)) {
+      const value = query.get(key)
+      if (value) handler(value)
+    }
+  }, [handlers, query])
+
+  const getQueryString = useCallback(() => {
+    const _query = new URLSearchParams()
+
+    for (const [key, value] of Object.entries(values)) {
+      if (value || value === 0) _query.set(key, String(value))
+    }
+
+    return _query.toString()
+  }, [values])
+
+  const replaceUrl = useCallback(() => {
+    router.push(`${pathname}?${getQueryString()}`, { scroll: false })
+  }, [getQueryString, pathname, router])
+
+  useEffect(() => {
+    replaceUrl()
+  }, [getQueryString, limit, offset, order, orderBy, pathname, replaceUrl, router])
+
+  useEffect(() => {
+    replaceUrl()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return {
     limit,
