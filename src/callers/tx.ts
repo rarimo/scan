41464d431a4c5bbdc5b7d686095815aa293e10b8
Phat <39623143+lukachi@users.noreply.github.com>
2024-03-20 13:12:34 +0200

    Fix `where` filter for tx list query (#7)
    * add back slash for plus symbol
    
    * remove / at the start & end of the regex
    
    * Update src/callers/tx.ts
    
    Co-authored-by: Semen Loktionov <47495186+napalmpapalam@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Semen Loktionov <47495186+napalmpapalam@users.noreply.github.com>

--- src/callers/tx.ts
+++ src/callers/tx.ts
@@ -20,11 +20,17 @@ import {
 const createWhereFilter = (sender: PublicKey | null, operator?: string) => {
   const exp = { signer_infos: {}, block: {} }
 
+  const escapedString = sender?.key.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+  const regex = new RegExp(escapedString!).toString()
+
+  // Remove the first and last character of the regex string to remove the / at the start and end
+  const regexString = regex.substring(1, regex.length - 1)
+
   if (sender) {
     exp.signer_infos = {
       _cast: {
         String: {
-          _regex: sender?.key,
+          _regex: regexString,
         },
       },
     }
